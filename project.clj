6ef9b50c6cd1320bc8d6173b0d90a5d60ef304c2
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-10-13 20:35:45 -0400

    Flesh out project.clj.

--- project.clj
+++ project.clj
@@ -1,13 +1,20 @@
 (defproject org.platypope/parenskit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure integration library for the LensKit framework."
+  :url "http://github.com/llasram/parenskit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :javac-options ["-target" "1.6" "-source" "1.6"]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.grouplens.lenskit/lenskit-data-structures "2.0.2"]
+                 [org.grouplens.lenskit/lenskit-api "2.0.2"]
                  [org.grouplens.lenskit/lenskit-core "2.0.2"]
                  [org.platypope/esfj "0.2.0"]]
-  :profiles {:java6
+  :profiles {:dev
+             {:dependencies
+              [[com.google.code.findbugs/annotations "2.0.2"]]}
+             :java6
              {:dependencies
               [[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]}})
